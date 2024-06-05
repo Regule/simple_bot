7063c677d0f660081f42642ebe3b8365f589eaef
Regule <regule@runbox.com>
2024-06-05 09:33:41 +0200

    Recuperar cambios en small_publisher

--- src/small_node_pub.cpp
+++ src/small_node_pub.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <string>
 #include <chrono>
+#include <sstream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -19,5 +20,20 @@ private:
   size_t counter_;
 
 private:
-  void timer_callback();
+  void timer_callback_();
 };
+
+SmallNodePublisher::SmallNodePublisher(): Node("small_node_publisher"), counter_(0){
+  publisher_ = this->create_publisher("small_node_topic", 10);
+  timer_ = this->create_wall_timer(chr::500ms, timer_callback_)
+}
+
+void SmallNodePublisher::timer_callback_(){
+  auto message = std_msgs::msg::String();
+  std:stringstream str_stream;
+  str_stream << "Message number " << counter_ << ".";
+  counter_++;
+  message.data = str_stream.str();
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  this->publisher_
+}
