366dda626d46a486f9ac6fdafe5f865997e22515
Regule <regule@runbox.com>
2024-06-05 11:00:34 +0200

    He preparado código a compilación.

--- src/small_node_pub.cpp
+++ src/small_node_pub.cpp
@@ -6,13 +6,13 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-namespace chr = std::chrono_literals;
+namespace chr = std::chrono;
 
 
 class SmallNodePublisher: public rclcpp::Node{
 
 public:
-  SmallNodePublisher()
+  SmallNodePublisher();
 
 private:
   rclcpp::TimerBase::SharedPtr timer_;
@@ -24,13 +24,13 @@ private:
 };
 
 SmallNodePublisher::SmallNodePublisher(): Node("small_node_publisher"), counter_(0){
-  publisher_ = this->create_publisher("small_node_topic", 10);
-  timer_ = this->create_wall_timer(chr::500ms, timer_callback_)
+  publisher_ = this->create_publisher<std_msgs::msg::String>("small_node_topic", 10);
+  timer_ = this->create_wall_timer(chr::milliseconds(500), std::bind(&SmallNodePublisher::timer_callback_, this));
 }
 
 void SmallNodePublisher::timer_callback_(){
   auto message = std_msgs::msg::String();
-  std:stringstream str_stream;
+  std::stringstream str_stream;
   str_stream << "Message number " << counter_ << ".";
   counter_++;
   message.data = str_stream.str();
@@ -40,7 +40,7 @@ void SmallNodePublisher::timer_callback_(){
 
 int main(int argc, char **argv){
   rclcpp::init(argc, argv);
-  auto node = SmallNodePublisher::make_shared("small_publisher")
+  auto node = SmallNodePublisher::make_shared("small_publisher");
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
