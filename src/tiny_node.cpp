02c7d69c861098ecb2b5c3383f8f8e6c9602e8ab
Regule <regule@runbox.com>
2024-06-06 14:13:21 +0200

    Añadí comentarios a tiny_node.

--- src/tiny_node.cpp
+++ src/tiny_node.cpp
@@ -1,11 +1,20 @@
+// Include the header file for the ROS 2 C++ library
 #include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char **argv){
-  rclcpp::init(argc, argv);
+// Entry point of the program
+int main(int argc, char **argv) {
+    // Initialize the ROS 2 node with the provided command line arguments
+    rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("tiny_node");
-  rclcpp::spin(node);
+    // Create a ROS 2 node with the name "tiny_node"
+    auto node = rclcpp::Node::make_shared("tiny_node");
 
-  rclcpp::shutdown();
-  return 0;
+    // Spin the node, i.e., keep it running and responsive to events
+    rclcpp::spin(node);
+
+    // Shutdown the ROS 2 node and release any associated resources
+    rclcpp::shutdown();
+
+    // Return 0 to indicate successful execution of the program
+    return 0;
 }
