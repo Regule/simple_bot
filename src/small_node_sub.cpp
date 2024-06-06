4dcea1d3c38289373f9a64d1438ef2edb50cd845
Regule <regule@runbox.com>
2024-06-06 14:16:20 +0200

    Añadí comentarios a subscriber.

--- src/small_node_sub.cpp
+++ src/small_node_sub.cpp
@@ -8,34 +8,40 @@
 
 using std::placeholders::_1;
 
-class SmallNodeSubscriber: public rclcpp::Node {
-
+// Define a class for the subscriber node
+class SmallNodeSubscriber : public rclcpp::Node {
 public:
-  SmallNodeSubscriber();
+  SmallNodeSubscriber(); // Constructor
 
 private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_; // Subscription to receive messages
 
 private:
-  void subscription_callback_(std_msgs::msg::String::UniquePtr msg);
+  void subscription_callback_(std_msgs::msg::String::UniquePtr msg); // Callback function for received messages
 };
 
-SmallNodeSubscriber::SmallNodeSubscriber(): Node("small_node_subscriber"){
-  subscription_ = this->create_subscription<std_msgs::msg::String>("small_node_topic", 10,
-                                                                   std::bind(&SmallNodeSubscriber::subscription_callback_, this, _1));
+// Constructor implementation
+SmallNodeSubscriber::SmallNodeSubscriber(): Node("small_node_subscriber") {
+  // Create a subscription to receive messages on the topic "small_node_topic"
+  subscription_ = this->create_subscription<std_msgs::msg::String>(
+    "small_node_topic",  // Topic name
+    10,                   // Queue size (maximum number of messages to be queued)
+    std::bind(&SmallNodeSubscriber::subscription_callback_, this, _1) // Callback function
+  );
 }
 
-void SmallNodeSubscriber::subscription_callback_(std_msgs::msg::String::UniquePtr msg){
-  RCLCPP_INFO(this->get_logger(), "Recieved message : '%s'", msg->data.c_str());
+// Callback function implementation
+void SmallNodeSubscriber::subscription_callback_(std_msgs::msg::String::UniquePtr msg) {
+  // Log the received message
+  RCLCPP_INFO(this->get_logger(), "Received message: '%s'", msg->data.c_str());
 }
 
 // Main function
 int main(int argc, char **argv) {
-
   // Initialize the ROS 2 runtime environment
   rclcpp::init(argc, argv);
 
-  // Create a shared pointer to an instance of SmallNodePublisher
+  // Create a shared pointer to an instance of SmallNodeSubscriber
   auto node = std::make_shared<SmallNodeSubscriber>();
 
   // Spin the node, allowing it to process callbacks until shutdown
